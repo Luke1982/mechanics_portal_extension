340c023c7a0aae178ea332c55d365512a0f5ec13
Luke1982 <gcx3@hotmail.com>
2017-09-05 17:26:48 +0200

    Use the USERS webservice ID in the creator

--- modules/MechanicsPortal/resources/handlers/getRegularTicketComments.php
+++ modules/MechanicsPortal/resources/handlers/getRegularTicketComments.php
@@ -5,7 +5,7 @@ function getRegularTicketComments($id) {
 	$comments = array();
 
 	list($wsid, $ticket_id) = explode('x', $id);
-	$q = "SELECT vtiger_modcomments.commentcontent, vtiger_crmentity.createdtime, vtiger_users.first_name AS creator_firstname, vtiger_users.last_name AS creator_lastname, vtiger_crmentity.smcreatorid AS creator FROM vtiger_modcomments LEFT JOIN vtiger_crmentity ON vtiger_modcomments.modcommentsid=vtiger_crmentity.crmid LEFT JOIN vtiger_users ON vtiger_crmentity.smcreatorid=vtiger_users.id WHERE vtiger_modcomments.related_to = ? AND vtiger_crmentity.deleted = ?";
+	$q = "SELECT vtiger_modcomments.commentcontent, vtiger_crmentity.createdtime, vtiger_users.first_name AS creator_firstname, vtiger_users.last_name AS creator_lastname, CONCAT('19x',vtiger_crmentity.smcreatorid) AS creator FROM vtiger_modcomments LEFT JOIN vtiger_crmentity ON vtiger_modcomments.modcommentsid=vtiger_crmentity.crmid LEFT JOIN vtiger_users ON vtiger_crmentity.smcreatorid=vtiger_users.id WHERE vtiger_modcomments.related_to = ? AND vtiger_crmentity.deleted = ?";
 	$r = $adb->pquery($q, array($ticket_id, 0));
 	while ($comment = $adb->fetch_array($r)) {
 		$comments[] = $comment;
